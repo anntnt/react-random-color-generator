0e7a177327908a00b45a7cecc326054c99e7029c
Ann Tran <anpjure@gmail.com>
2026-04-16 12:23:10 +0200

    Fix tests and add missing Jest environment packages

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import { expect, test } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders random color generator heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(
+    screen.getByRole('heading', { name: /random color generator/i }),
+  ).toBeInTheDocument();
 });
