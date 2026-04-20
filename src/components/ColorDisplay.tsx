76dfd9c1352926e56c3a48f8abf26fe4aa5b9a73
Ann Tran <anpjure@gmail.com>
2026-04-20 17:12:45 +0200

    Add the transition smoothly between the colors as they change

--- src/components/ColorDisplay.tsx
+++ src/components/ColorDisplay.tsx
@@ -9,6 +9,7 @@ export function ColorDisplay({ color, size }: ColorDisplayProps) {
       className="box-border flex flex-col items-center justify-center border-[5px] border-dotted border-[#666666] text-center"
       style={{
         backgroundColor: color,
+        transition: "background-color 1.5s ease",
         width: `${size}px`,
         height: `${size}px`,
       }}
